f1eeb72ae0cd59e5fe30ee583c924e4acf3f8fd2
rudra Tripathy <tripathyrudra470@gmail.com>
2025-11-27 14:37:39 +0000

    remove this

--- src/components/ui/pill-nav.tsx
+++ src/components/ui/pill-nav.tsx
@@ -271,20 +271,13 @@ const PillNav: React.FC<PillNavProps> = ({
             onMouseLeave={handleLogoLeave}
             role="menuitem"
             ref={logoRef}
-            className="rounded-full px-4 py-2 inline-flex items-center justify-center overflow-hidden font-headline font-bold text-xl"
+            className="rounded-full px-4 py-2 inline-flex items-center justify-center overflow-hidden font-headline font-bold text-2xl text-white"
             style={{
               height: 'var(--nav-h)',
               background: 'transparent',
-              color: 'var(--base, #000)'
             }}
           >
-           <GradientText
-              colors={["#29ABE2", "#F9A825", "#B19EEF", "#29ABE2"]}
-              animationSpeed={5}
-              className="font-headline text-2xl font-bold"
-            >
-              {logoText}
-            </GradientText>
+           {logoText}
         </Link>
         
 
